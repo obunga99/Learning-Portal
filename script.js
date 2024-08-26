210dc4364c71a9870204f1cf5973617a63ca9a34
obunga99 <110899027+obunga99@users.noreply.github.com>
2024-08-27 06:40:45 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var but = document.getElementById("but");
+var para10 = document.getElementById("para10Text2");
+
+var but2 = document.getElementById("but2");
+var paraText = document.getElementById("paraText");
+
+var but3 =  document.getElementById("but3");
+var paraText2 = document.getElementById("statSpan");
+
+
+function butAct(){
+    but.style.visibility = "hidden";
+    para10.style.visibility = "visible";
+    
+}
+function butAct2(){
+    but2.style.visibility = "hidden";
+    paraText.style.visibility = "visible";
+    
+}
+
+function butAct3(){
+    but3.style.visibility = "hidden";
+    paraText2.style.visibility = "visible";
+    
+}
